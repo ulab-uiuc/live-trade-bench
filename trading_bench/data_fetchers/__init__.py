7ca43942fa84b87717cd191876ffe561cc920ba5
Haofei Yu <1125027232@qq.com>
2025-07-09 19:09:22 -0700

    update

--- trading_bench/data_fetchers/__init__.py
+++ trading_bench/data_fetchers/__init__.py
@@ -2,70 +2,14 @@
 Data fetchers module for trading bench.
 
 This module contains specialized data fetchers for different data sources:
-- stock_fetcher: Stock price data from Yahoo Finance
-- option_fetcher: Option data from Yahoo Finance
+- stock_fetcher: Stock price data from Yahoo Finance  
 - news_fetcher: News data from Google News
-- polymarket_fetcher: Prediction market data from Polymarket
-- reddit_fetcher: Reddit posts and comments data from local JSONL files
 """
 
 from trading_bench.data_fetchers.news_fetcher import fetch_news_data
-from trading_bench.data_fetchers.option_fetcher import (
-    calculate_implied_volatility,
-    calculate_option_greeks,
-    fetch_option_chain,
-    fetch_option_data,
-    fetch_option_expirations,
-    fetch_option_historical_data,
-    get_atm_options,
-    get_option_chain_summary,
-)
-from trading_bench.data_fetchers.polymarket_fetcher import (
-    fetch_polymarket_market_details,
-    fetch_polymarket_market_stats,
-    fetch_polymarket_markets,
-    fetch_polymarket_orderbook,
-    fetch_polymarket_trades,
-    fetch_polymarket_trending_markets,
-    search_polymarket_markets,
-)
-from trading_bench.data_fetchers.reddit_fetcher import (
-    fetch_reddit_posts_by_ticker,
-    fetch_reddit_sentiment_data,
-    fetch_top_from_category,
-    get_available_categories,
-    get_available_dates,
-    get_reddit_statistics,
-)
 from trading_bench.data_fetchers.stock_fetcher import fetch_price_data
 
 __all__ = [
-    # Stock data
     'fetch_price_data',
-    # Option data
-    'fetch_option_chain',
-    'fetch_option_data',
-    'fetch_option_expirations',
-    'fetch_option_historical_data',
-    'calculate_option_greeks',
-    'get_atm_options',
-    'calculate_implied_volatility',
-    'get_option_chain_summary',
-    # News data
     'fetch_news_data',
-    # Polymarket data
-    'fetch_polymarket_markets',
-    'fetch_polymarket_market_details',
-    'fetch_polymarket_orderbook',
-    'fetch_polymarket_trades',
-    'fetch_polymarket_market_stats',
-    'search_polymarket_markets',
-    'fetch_polymarket_trending_markets',
-    # Reddit data
-    'fetch_top_from_category',
-    'fetch_reddit_posts_by_ticker',
-    'fetch_reddit_sentiment_data',
-    'get_available_categories',
-    'get_available_dates',
-    'get_reddit_statistics',
 ]
