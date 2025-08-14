224805ba852dc0eaedaf82135f7d702ad52b8198
Haofei Yu <1125027232@qq.com>
2025-08-13 17:00:19 -0700

    feat(accounts): rename account modules (#44)
    <!--
    Thanks for creating this pull request 🤗
    Please make sure that the pull request is limited to one type (docs,
    feature, etc.) and keep it as small as possible. You can open multiple
    prs instead of opening a huge one.
    -->
    
    <!-- If this pull request closes an issue, please mention the issue
    number below -->
    
    Closes # <!-- Issue # here -->
    
    ## 📑 Description
    
    <!-- Add a brief description of the pr -->
    
    <!-- You can also choose to add a list of changes and if they have been
    completed or not by using the markdown to-do list syntax
    - [ ] Not Completed
    - [x] Completed
    -->
    
    ## ✅ Checks
    
    <!-- Make sure your pr passes the CI checks and do check the following
    fields as needed - -->
    
    - [ ] My pull request adheres to the code style of this project
    - [ ] My code requires changes to the documentation
    - [ ] I have updated the documentation as required
    - [ ] All the tests have passed
    - [ ] Branch name follows `type/descript` (e.g.
    `feature/add-llm-agents`)
    - [ ] Ready for code review
    
    ## ℹ Additional Information
    
    <!-- Any additional information like breaking changes, dependencies
    added, screenshots, comparisons between new and old behavior, etc. -->

---
+++ trading_bench/accounts/__init__.py
@@ -0,0 +1,39 @@
+"""
+Trading Bench Evaluators Package
+"""
+
+from .action import PolymarketAction, StockAction
+from .base_account import BaseAccount
+from .polymarket_account import (
+    PolymarketAccount,
+    PolymarketPosition,
+    PolymarketTransaction,
+    create_polymarket_account,
+    eval_polymarket_account,
+)
+from .stock_account import (
+    StockAccount,
+    StockPosition,
+    StockTransaction,
+    create_stock_account,
+    eval_account,
+)
+
+__all__ = [
+    # Base classes
+    "BaseAccount",
+    # Stock trading
+    "StockAccount",
+    "StockPosition",
+    "StockTransaction",
+    "StockAction",
+    "create_stock_account",
+    "eval_account",
+    # Polymarket trading
+    "PolymarketAccount",
+    "PolymarketPosition",
+    "PolymarketTransaction",
+    "PolymarketAction",
+    "create_polymarket_account",
+    "eval_polymarket_account",
+]
