cfad0bf6d72faeead7029c136bcef35de0a01421
Haofei Yu <1125027232@qq.com>
2025-08-27 17:08:52 -0500

    feat(app): fully support frontend (#80)
    <!--
    Thanks for creating this pull request 🤗
    Please make sure that the pull request is limited to one type (docs,
    feature, etc.) and keep it as small as possible. You can open multiple
    prs instead of opening a huge one.
    -->
    
    <!-- If this pull request closes an issue, please mention the issue
    number below -->
    
    Closes # <!-- Issue # here -->
    
    ## 📑 Description
    
    <!-- Add a brief description of the pr -->
    
    <!-- You can also choose to add a list of changes and if they have been
    completed or not by using the markdown to-do list syntax
    - [ ] Not Completed
    - [x] Completed
    -->
    
    ## ✅ Checks
    
    <!-- Make sure your pr passes the CI checks and do check the following
    fields as needed - -->
    
    - [ ] My pull request adheres to the code style of this project
    - [ ] My code requires changes to the documentation
    - [ ] I have updated the documentation as required
    - [ ] All the tests have passed
    - [ ] Branch name follows `type/descript` (e.g.
    `feature/add-llm-agents`)
    - [ ] Ready for code review
    
    ## ℹ Additional Information
    
    <!-- Any additional information like breaking changes, dependencies
    added, screenshots, comparisons between new and old behavior, etc. -->

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,7 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
