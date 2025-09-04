c2095a80a89b79d0d9f0ac67a011534de3caee8b
4R5T <maxmeetsli@gmail.com>
2025-09-04 18:15:24 -0500

    feat(accounts): using real data instead of simulate

--- backend/app/main.py
+++ backend/app/main.py
@@ -358,4 +358,4 @@ async def serve_frontend(full_path: str) -> Any:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5001)
